495128beca8afa6e2d53f78ae33d37c797077d29
menpj <3994084+menpj@users.noreply.github.com>
2024-04-17 14:34:37 +0530

    random message

---
+++ encyclopedia/templates/temp.py
@@ -0,0 +1,1 @@
+<!--form action="{% url 'encyclopedia:editPage' name={{name}} %}" method="post" -->
---
+++ encyclopedia/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name= "encyclopedia"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    #path("content",views.content,name="content"),
+    path("wiki",views.wiki,name="wiki"),
+    path("wiki/<str:name>",views.wiki,name="wiki"),
+    path("wikiSearch",views.wikiSearch,name="wikiSearch"),
+    path("newPage",views.newPage,name="newPage"),
+    path("newPageError",views.newPageError,name="newPageError"),
+    path("editPage/<str:name>",views.editPage,name="editPage"),
+    path("editPage",views.editPage,name="editPage")
+    
+]
